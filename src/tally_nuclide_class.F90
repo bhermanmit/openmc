b3f94d8350904e83f2f4c1a4a56fb24bc62abfd0
Bryan Herman <bherman@mit.edu>
2014-07-28 10:28:02 -0400

    added tally nuclide class

---
+++ src/tally_nuclide_class.F90
@@ -0,0 +1,14 @@
+module tally_nuclide_class
+
+  use particle_header, only: Particle
+
+  implicit none
+  private
+
+  ! General tally score type
+  type, public :: TallyNuclideClass
+    private
+    contains
+  end type TallyNuclideClass
+
+end module tally_nuclide_class
