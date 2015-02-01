4d3b58bb58a5b057b18920565520d6908d34f412
Bryan Herman <bherman@mit.edu>
2015-02-01 11:22:40 -0500

    fixed travis pull request var

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -3,7 +3,7 @@
 set -ev
 
 # Build HDF5 and PETSc for rest of debug tests
-if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" ]; then
 
   # Build MPICH
   wget -q http://www.mpich.org/static/downloads/3.1.3/mpich-3.1.3.tar.gz
