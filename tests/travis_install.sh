8ee7d096e5862face8acdb0e8230d5c0edf7e9ed
Bryan Herman <bherman@mit.edu>
2015-01-31 23:00:22 -0500

    fixed hdf5 dir

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -9,17 +9,17 @@ wget -q http://www.mpich.org/static/downloads/3.1.3/mpich-3.1.3.tar.gz
 tar -xzvf mpich-3.1.3.tar.gz >/dev/null 2>&1
 cd mpich-3.1.3
 ./configure --prefix=$PWD/../mpich_install -q
-make -j -s --quiet
-make -s --quiet install
+make -j >/dev/null 2>&1
+make install >/dev/null 2>&1
 cd ..
 
 # Build PHDF5
 wget -q http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.14.tar.gz
 tar -xzvf hdf5-1.8.14.tar.gz >/dev/null 2>&1
-cd  hdf5-1.8.14; mv hdf5-1.8.14 phdf5-1.8.14
+mv hdf5-1.8.14 phdf5-1.8.14; cd phdf5-1.8.14
 CC=../mpich_install/bin/mpicc FC=../mpich_install/bin/mpif90 ./configure --prefix=$PWD/../phdf5_install -q --enable-fortran --enable-fortran2003 --enable-parallel
-make -j -s --quiet
-make -s --quiet install
+make -j >/dev/null 2>&1
+make install >/dev/null 2>&1
 cd ..
 
 # Build HDF5 and PETSc for rest of debug tests
@@ -29,8 +29,8 @@ if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
   tar -xzvf hdf5-1.8.14.tar.gz >/dev/null 2>&1
   cd  hdf5-1.8.14
   CC=gcc FC=gfortran ./configure --prefix=$PWD/../hdf5_install -q --enable-fortran --enable-fortran2003
-  make -j -s --quiet
-  make -s --quiet install
+  make -j >/dev/null 2>&1
+  make install >/dev/null 2>&1
   cd ..
 
 fi
