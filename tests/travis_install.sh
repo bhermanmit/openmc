46d8557b61ece7c3d33cb8518a45c9aae18b3245
Bryan Herman <bherman@mit.edu>
2015-01-31 23:11:46 -0500

    absolute paths for compilers

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -17,8 +17,8 @@ cd ..
 wget -q http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.14.tar.gz
 tar -xzvf hdf5-1.8.14.tar.gz >/dev/null 2>&1
 mv hdf5-1.8.14 phdf5-1.8.14; cd phdf5-1.8.14
-CC=../mpich_install/bin/mpicc FC=../mpich_install/bin/mpif90 ./configure --prefix=$PWD/../phdf5_install -q --enable-fortran --enable-fortran2003 --enable-parallel
-make -j
+CC=$PWD/../mpich_install/bin/mpicc FC=$PWD/../mpich_install/bin/mpif90 ./configure --prefix=$PWD/../phdf5_install -q --enable-fortran --enable-fortran2003 --enable-parallel
+make -j >/dev/null 2>&1
 make install >/dev/null 2>&1
 cd ..
 
