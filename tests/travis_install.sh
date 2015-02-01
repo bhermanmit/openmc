6c891bff6f44fc4a3a58577a4d82bc9bcc60f08f
Bryan Herman <bherman@mit.edu>
2015-01-31 22:47:14 -0500

    added dev null for tar

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -6,7 +6,7 @@ set -ev
 
 # Build MPICH
 wget -q http://www.mpich.org/static/downloads/3.1.3/mpich-3.1.3.tar.gz
-tar -xzvf mpich-3.1.3.tar.gz
+tar -xzvf mpich-3.1.3.tar.gz >/dev/null 2>&1
 cd mpich-3.1.3
 ./configure --prefix=$PWD/../mpich_install -q
 make -j -s
@@ -16,7 +16,7 @@ rm -rf mpich-3.1.3
 
 # Build PHDF5
 wget -q http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.14.tar.gz
-tar -xzvf hdf5-1.8.14.tar.gz
+tar -xzvf hdf5-1.8.14.tar.gz >/dev/null 2>&1
 cd  hdf5-1.8.14
 CC=../mpich_install/bin/mpicc FC=../mpich_install/bin/mpif90 ./configure --prefix=$PWD/../phdf5_install -q --enable-fortran --enable-fortran2003 --enable-parallel
 make -j -s
@@ -28,7 +28,7 @@ rm -rf hdf5-1.8.14
 if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
 
   # Build HDF5
-  tar -xzvf hdf5-1.8.14.tar.gz
+  tar -xzvf hdf5-1.8.14.tar.gz >/dev/null 2>&1
   cd  hdf5-1.8.14
   CC=gcc FC=gfortran ./configure --prefix=$PWD/../hdf5_install -q --enable-fortran --enable-fortran2003
   make -j -s
