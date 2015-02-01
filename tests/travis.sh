ab0a911d45e3487d0ed4f1c66e97d43bf55e866a
Bryan Herman <bherman@mit.edu>
2015-02-01 10:54:45 -0500

    added more complicated testing for pull requests

---
+++ tests/travis.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -ev
+
+# Run all debug tests
+if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
+  ./run_tests.py -C "basic-debug|hdf5-debug|mpi-omp-debug|phdf5-omp-debug" -j 4
+else
+  ./run_tests.py -C "basic-debug" -j 4
+fi
--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -2,28 +2,27 @@
 
 set -ev
 
-# For every CI, we will at least run phdf5-omp-debug
-
-# Build MPICH
-wget -q http://www.mpich.org/static/downloads/3.1.3/mpich-3.1.3.tar.gz
-tar -xzvf mpich-3.1.3.tar.gz >/dev/null 2>&1
-cd mpich-3.1.3
-./configure --prefix=$PWD/../mpich_install -q
-make -j >/dev/null 2>&1
-make install >/dev/null 2>&1
-cd ..
-
-# Build PHDF5
-wget -q http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.14.tar.gz
-tar -xzvf hdf5-1.8.14.tar.gz >/dev/null 2>&1
-mv hdf5-1.8.14 phdf5-1.8.14; cd phdf5-1.8.14
-CC=$PWD/../mpich_install/bin/mpicc FC=$PWD/../mpich_install/bin/mpif90 ./configure --prefix=$PWD/../phdf5_install -q --enable-fortran --enable-fortran2003 --enable-parallel
-make -j >/dev/null 2>&1
-make install >/dev/null 2>&1
-cd ..
-
 # Build HDF5 and PETSc for rest of debug tests
 if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
+
+  # Build MPICH
+  wget -q http://www.mpich.org/static/downloads/3.1.3/mpich-3.1.3.tar.gz
+  tar -xzvf mpich-3.1.3.tar.gz >/dev/null 2>&1
+  cd mpich-3.1.3
+  ./configure --prefix=$PWD/../mpich_install -q
+  make -j >/dev/null 2>&1
+  make install >/dev/null 2>&1
+  cd ..
+
+  # Build PHDF5
+  wget -q http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.14.tar.gz
+  tar -xzvf hdf5-1.8.14.tar.gz >/dev/null 2>&1
+  mv hdf5-1.8.14 phdf5-1.8.14; cd phdf5-1.8.14
+  CC=$PWD/../mpich_install/bin/mpicc FC=$PWD/../mpich_install/bin/mpif90 ./configure \
+     --prefix=$PWD/../phdf5_install -q --enable-fortran --enable-fortran2003 --enable-parallel
+  make -j >/dev/null 2>&1
+  make install >/dev/null 2>&1
+  cd ..
 
   # Build HDF5
   tar -xzvf hdf5-1.8.14.tar.gz >/dev/null 2>&1
